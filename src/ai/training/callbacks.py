065e0138e5bddec660e7358517aab5463d8b185e
KOSASIH <kosasihg88@gmail.com>
2025-02-25 21:13:38 +0700

    Create callbacks.py

---
+++ src/ai/training/callbacks.py
@@ -0,0 +1,24 @@
+import torch
+
+class EarlyStopping:
+    def __init__(self, patience: int = 3, verbose: bool = False):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_loss = None
+        self.early_stop = False
+
+    def __call__(self, val_loss, model):
+        if self.best_loss is None:
+            self.best_loss = val_loss
+        elif val_loss > self.best_loss:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_loss = val_loss
+            self.counter = 0
+            # Save the model if needed
+            torch.save(model.state_dict(), 'best_model.pth')
