7a8957edcdb7d4e0ccbda30567276e4938067296
KOSASIH <kosasihg88@gmail.com>
2025-02-26 08:57:42 +0700

    Create __init__.py

---
+++ src/quantum/quantumAlgorithms/__init__.py
@@ -0,0 +1,9 @@
+# quantum/quantumAlgorithms/__init__.py
+
+from .quantum_algorithm_1 import run_quantum_algorithm_1
+from .quantum_algorithm_2 import run_quantum_algorithm_2
+
+__all__ = [
+    "run_quantum_algorithm_1",
+    "run_quantum_algorithm_2"
+]
