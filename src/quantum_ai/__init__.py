506be43adcea01459b629caec6259f27ba2d397e
KOSASIH <kosasihg88@gmail.com>
2025-02-27 10:18:06 +0700

    Create __init__.py

---
+++ src/quantum_ai/__init__.py
@@ -0,0 +1,9 @@
+# src/quantum_ai/__init__.py
+
+__all__ = [
+    "quantum_machine_learning",
+    "generative_quantum_ai",
+    "hybrid_quantum_classical",
+    "quantum_nlp",
+    "quantum_cryptography"
+]
